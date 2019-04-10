64d156ca98db3570a433a51f7dc559f58a1256d0
Simon Maher <simon.maher@integrify.io>
2019-04-10 13:46:57 +0300

    created const, created event listener

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const previousButton = document.querySelector('.previous-button')
+const nextButton = document.querySelector('.next-button')
+const moreButton = document.querySelector('.more-button')
+// ====
+// Add event listener for all buttons
+previousButton.addEventListener('click')
+nextButton.addEventListener('click')
+moreButton.addEventListener('click')
+// Add const (delete later if not needed)
+const imageDiv = document.querySelector('.image-div')
+const defaultInfoDiv = document.querySelector('.default-info-div')
+const titleP = document.querySelector('.title-p')
+const moreInfoDiv = document.querySelector('.more-info-div')
+// ====
+const nationalityP = document.querySelector('.nationality-p')
+const nationalitySpan = document.querySelector('.nationality-span')
+// ====
+const skillsP = document.querySelector('.skills-p')
+const skillsSpan = document.querySelector('.skills-span')
+// ====
+const driveP = document.querySelector('.drive-p')
+const driveSpan = document.querySelector('.drive-span')
+// ====
+const visionP = document.querySelector('vision-p')
+const visionSpan = document.querySelector('.vision-span')
+// ====
+const motivationP = document.querySelector('.motivation-p')
+const motivationSpan = document.querySelector('.motivation-span')
+// ====
+const quoteP = document.querySelector('.quote-p')
+
