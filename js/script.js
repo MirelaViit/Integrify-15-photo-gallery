3603b455ec5581a7d9eec17953b7d837a251ee6e
Simon Maher <simon.maher@integrify.io>
2019-04-10 15:28:24 +0300

    added first object

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ const nextButton = document.querySelector('.next-button')
 const moreButton = document.querySelector('.more-button')
 // ====
 // Add event listener for all buttons
-previousButton.addEventListener('click')
-nextButton.addEventListener('click')
-moreButton.addEventListener('click')
+previousButton.addEventListener('click', function () { })
+nextButton.addEventListener('click', function () { })
+moreButton.addEventListener('click', function () { })
 // Add const (delete later if not needed)
 const imageDiv = document.querySelector('.image-div')
+const imageImg = document.querySelector('.image-img')
 const defaultInfoDiv = document.querySelector('.default-info-div')
+const nameP = document.querySelector('.name-p')
 const titleP = document.querySelector('.title-p')
 const moreInfoDiv = document.querySelector('.more-info-div')
 // ====
@@ -29,3 +31,41 @@ const motivationSpan = document.querySelector('.motivation-span')
 // ====
 const quoteP = document.querySelector('.quote-p')
 
+// console.log(studentInfo)
+// console.log(studentInfo[0])
+
+function generateStudent(object) {
+    const name = object.firstName + '' + object.lastName;
+    nameP.innerHTML = name;
+
+    const title = object.title;
+    titleP.innerHTML = title;
+
+    const nationality = object.nationality;
+    nationalitySpan.innerHTML = nationality;
+
+    const skills = object.skills
+    skillsSpan.innerHTML = skills;
+
+    src = './assets/' + object.src;
+    imageImg.src = src;
+
+    const vision = object.longTermVision;
+    visionSpan.innerHTML = vision
+
+    const motivation = object.motivatesMe;
+    motivationSpan.innerHTML = motivation
+    const quote = object.favoriteQuote;
+    quoteP.innerHTML = quote;
+
+    console.log(name, title);
+    return;
+}
+
+
+
+function initialize() {
+    generateStudent(studentInfo[0]);
+}
+
+initialize();
