354277d8c55b6792866d45837a7611e1684ab6fe
Xurxe Toivo García <xurxe.garcia@integrify.io>
2019-04-10 17:08:45 +0300

    Script1 (#2)
    * created const, created event listener
    
    * added first object
    
    * added display more information function
    
    * addednew function

---
+++ js/script.js
@@ -0,0 +1,88 @@
+const previousButton = document.querySelector('.previous-button')
+const nextButton = document.querySelector('.next-button')
+const moreButton = document.querySelector('.more-button')
+// ====
+// Add event listener for all buttons
+previousButton.addEventListener('click', function () { })
+nextButton.addEventListener('click', function () { })
+
+// Function that hides and displays the 'more info' section
+moreButton.addEventListener('click', function () {
+    if (clickState === 0) {
+        document.querySelector('.more-info-div').style.display = 'block'
+        clickState = 1
+    } else {
+        document.querySelector('.more-info-div').style.display = 'none'
+        clickState = 0
+    }
+})
+// Add const (delete later if not needed)
+const imageDiv = document.querySelector('.image-div')
+const imageImg = document.querySelector('.image-img')
+const defaultInfoDiv = document.querySelector('.default-info-div')
+const nameP = document.querySelector('.name-p')
+const titleP = document.querySelector('.title-p')
+const moreInfoDiv = document.querySelector('.more-info-div')
+// ====
+const nationalityP = document.querySelector('.nationality-p')
+const nationalitySpan = document.querySelector('.nationality-span')
+// ====
+const skillsP = document.querySelector('.skills-p')
+const skillsSpan = document.querySelector('.skills-span')
+// ====
+const driveP = document.querySelector('.drive-p')
+const driveSpan = document.querySelector('.drive-span')
+// ====
+const visionP = document.querySelector('vision-p')
+const visionSpan = document.querySelector('.vision-span')
+// ====
+const motivationP = document.querySelector('.motivation-p')
+const motivationSpan = document.querySelector('.motivation-span')
+// ====
+const quoteP = document.querySelector('.quote-p')
+let clickState = 0;
+let index = 0;
+// console.log(studentInfo)
+// console.log(studentInfo[0])
+// =======================================================================================
+function generateStudent(i) {
+    const name = studentInfo[i].firstName + '' + studentInfo[i].lastName;
+    nameP.innerHTML = name;
+
+    const title = studentInfo[i].title;
+    titleP.innerHTML = title;
+
+    const nationality = studentInfo[i].nationality;
+    nationalitySpan.innerHTML = nationality;
+
+    const skills = studentInfo[i].skills
+    skillsSpan.innerHTML = skills.join(', ');
+
+    src = './assets/' + studentInfo[i].src;
+    imageImg.src = src;
+
+    const vision = studentInfo[i].longTermVision;
+    visionSpan.innerHTML = vision
+
+    const motivation = studentInfo[i].motivatesMe;
+    motivationSpan.innerHTML = motivation
+    const quote = studentInfo[i].favoriteQuote;
+    quoteP.innerHTML = quote;
+
+    console.log(name, title);
+    return;
+}
+
+
+
+function initialize() {
+    generateStudent(index)
+    for (let i = 0; i < studentInfo.length; i++) {
+        if (i = studentInfo.length) {
+            index = 0
+        }
+    };
+    console.log(studentInfo[5])
+}
+
+initialize();
